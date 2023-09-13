e0deb6855a717567319f0b041152f8567f2f9bff
José Eduardo <joseeduardo0131@gmail.com>
2023-09-12 21:58:00 -0300

    acessando banco de dados com dapper

---
+++ Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ Screens/TagScreens/DeleteTagScreen.cs
@@ -0,0 +1,41 @@
+﻿using Blog.Models;
+using Blog.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Screens.TagScreens
+{
+    public class DeleteTagScreen
+    {
+        public static void Load()
+        {
+            Console.Clear();
+            Console.WriteLine("Deletando uma tag");
+            Console.WriteLine("Qual o Id da tag que deseja excluir? ");
+            var id = Console.ReadLine();
+
+            Console.WriteLine("-------------");
+            Delete(int.Parse(id));
+            Console.ReadKey();
+            MenuTagScreen.Load();
+        }
+
+        public static void Delete(int id)
+        {
+            try
+            {
+                var repository = new Repository<Tag>(Database.Connection);
+                repository.Delete(id);
+                Console.WriteLine("Tag removida com sucesso");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Não foi possível remover a tag");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
